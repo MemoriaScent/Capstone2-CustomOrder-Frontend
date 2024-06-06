4a36e2544f56d9d4d7ea1103e41ff64814dbdd0f
Hun3431 <limdohun77@gmail.com>
2024-06-07 03:33:38 +0900

    [Feat] Product 추가

--- src/components/molecule/ProductOrder/index.tsx
+++ src/components/molecule/ProductOrder/index.tsx
@@ -8,40 +8,52 @@ import React, { useState } from "react";
 const ProductOrder: React.FC<ProductDetailsProps> = ({ params }) => {
   const router = useRouter();
   // 상품 조회 정보?
-  const productId = params.productId;
+  const productId = Number(params.productId);
   const [count, setCount] = useState(1);
-  const Product = {
-    name: "Signature Diffuser - forest",
-    volume: 200,
-    price: 40000,
-  };
+  const Product = [
+    {
+      name: "Signature Diffuser - forest",
+      volume: 200,
+      price: 40000,
+    },
+    {
+      name: "Signature Diffuser - cotton candy",
+      volume: 200,
+      price: 40000,
+    },
+    {
+      name: "Signature Diffuser - marine",
+      volume: 200,
+      price: 40000,
+    },
+  ];
   return (
     <div className="flex flex-col">
       <Label className="pretendardSemiBoldFont-24">
-        Signature Diffuser - forest
+        {Product[productId].name}
       </Label>
       <Label className="pretendardNormalFont-20 mt-2">
-        {Product.volume + "ml"}
+        {Product[productId].volume + "ml"}
       </Label>
       <Label className="pretendardNormalFont-20 mt-10">
-        {Product.price.toLocaleString("ko-KR") + "원"}
+        {Product[productId].price.toLocaleString("ko-KR") + "원"}
       </Label>
       <Label className="pretendardNormalFont-16 mt-10">수량</Label>
-      <div className="flex flex-row items-center w-120 h-30 border border-black divide-x divide-black pretendardNormalFont-16 mt-2">
+      <div className="flex flex-row items-center justify-between w-110 h-30 border border-black divide-x divide-black pretendardNormalFont-16 mt-2 p-0">
         <Button
           onClick={() => {
             if (count > 1) setCount(count - 1);
           }}
-          className="w-10 rounded-none"
+          className="w-10 rounded-none text-center"
         >
           -
         </Button>
-        <Label className="w-10 text-center">{count}</Label>
+        <Label className="w-full text-center">{count}</Label>
         <Button
           onClick={() => {
             if (count < 20) setCount(count + 1);
           }}
-          className="w-10 rounded-none"
+          className="w-10 rounded-none  text-center"
         >
           +
         </Button>
@@ -55,14 +67,16 @@ const ProductOrder: React.FC<ProductDetailsProps> = ({ params }) => {
       </div>
       <div className="flex flex-row justify-between pretendardNormalFont-18 mt-4">
         <Label>총 주문 금액</Label>
-        <Label>{(count * Product.price).toLocaleString("ko-KR") + "원"}</Label>
+        <Label>
+          {(count * Product[productId].price).toLocaleString("ko-KR") + "원"}
+        </Label>
       </div>
       <div className="flex flex-col mt-8">
         <Button
           className="h-50 bg-custom-orange text-white rounded-none"
           onClick={() => {
             router.push(
-              `/payment?name=${Product.name}&count=${count}&price=${Product.price}`
+              `/payment?name=${Product[productId].name}&count=${count}&price=${Product[productId].price}`
             );
           }}
         >
