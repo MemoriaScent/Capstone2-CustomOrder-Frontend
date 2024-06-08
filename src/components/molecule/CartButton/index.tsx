675cedf48611dcf7e0b4cb9a33b5b9c20f9886d3
Hun3431 <limdohun77@gmail.com>
2024-06-08 22:20:58 +0900

    [Feat] Cart 이동 추가

--- src/components/molecule/CartButton/index.tsx
+++ src/components/molecule/CartButton/index.tsx
@@ -1,13 +1,18 @@
 import Button from "@/components/atoms/Button";
 import { BasePorps } from "@/components/Types";
 import { orangeColorStyle } from "@/styles/color";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const CartButton: React.FC<BasePorps> = ({ children, css, className }) => {
+  const router = useRouter();
   return (
     <div style={{ ...CartButtonStyle, ...orangeColorStyle }}>
       <Button
-        onClick={() => {}}
+        onClick={(event) => {
+          event.stopPropagation;
+          router.push("/cart");
+        }}
         className={"flex justify-center items-center w-full h-full"}
       >
         cart
