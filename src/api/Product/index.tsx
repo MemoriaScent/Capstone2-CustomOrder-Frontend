ccaef23e7f4ce2f0714f414fbe690332d24cc74f
Hun3431 <limdohun77@gmail.com>
2024-06-11 13:23:47 +0900

    배포 테스트 진행

--- src/api/Product/index.tsx
+++ src/api/Product/index.tsx
@@ -1,5 +1,10 @@
+require("dotenv").config();
+
 const getProductData = async () => {
-  const response = await fetch("http://113.198.229.155:8001/deffuser");
+  const response = await fetch(
+    // 로컬에서 실행 시 SERVER -> LOCAL 변경하여 사용
+    `${process.env.API_SERVER_URL}:${process.env.API_SERVER_PORT}/deffuser`
+  );
   if (!response.ok) {
     throw new Error("Failed to fetch data");
   }
