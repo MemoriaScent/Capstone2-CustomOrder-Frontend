bda86d0f9b3d8cc1f994b8b57ff0eeb9d7e7931a
Hun3431 <limdohun77@gmail.com>
2024-06-14 05:39:06 +0900

    임시 저장

--- src/components/templates/CustomSuccessTemplate/index.tsx
+++ src/components/templates/CustomSuccessTemplate/index.tsx
@@ -1,10 +1,75 @@
 "use client";
 import Custom6 from "@/components/organism/Custom6";
 import CustomBGImage from "@/components/organism/CustomBGImage";
+import { useAtomValue } from "jotai";
 import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { customAtom } from "../CustomTemplate/JAtom";
+import getGPTData from "@/api/GPT";
 
-const CustomSuccessTemplate = () => {
+const CustomSuccessTemplate = async () => {
   const router = useRouter();
+  const [responseData, setResponseData] = useState();
+  const [loading, setLoading] = useState<boolean>(true);
+
+  // GPT API 호출
+  const custom = useAtomValue(customAtom);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (!custom.story && !custom.taste) {
+        console.log("no text");
+        return;
+      } else if (!custom.image) {
+        console.log("no image");
+        return;
+      } else {
+        try {
+          const response = await getGPTData(
+            custom.story + custom.taste,
+            custom.image
+          );
+
+          console.log("---------------");
+
+          const like: string[] = [];
+          const hate: string[] = [];
+
+          if (response) {
+            const dataObject = response["선호도"];
+
+            for (const key in dataObject) {
+              if (dataObject.hasOwnProperty(key)) {
+                const value = dataObject[key];
+                if (value.Level === 1 || value.Level === 2) {
+                  like.push(key);
+                } else if (value.Level === 0) {
+                  hate.push(key);
+                }
+              }
+            }
+            const res = { like, hate };
+            setResponseData(res);
+            useState(false);
+          }
+        } catch (error) {
+          console.error("API 호출 오류:", error);
+        }
+      }
+    };
+
+    fetchData();
+  }, [custom]);
+
+  if (loading) {
+    return (
+      <div className="flex flex-col justify-center items-center">
+        <div>Loading</div>
+      </div>
+    );
+  }
+
+  if (responseData) console.log(responseData.like);
   const complete = () => {
     router.push("/custom/complete");
   };
@@ -14,7 +79,7 @@ const CustomSuccessTemplate = () => {
         <CustomBGImage />
         <div className="absolute w-full h-full flex justify-center">
           <div className=" flex flex-col items-center w-1300 h-45vw border-x border-t border-black bg-white">
-            <Custom6 complete={complete}></Custom6>
+            <Custom6 complete={complete} note={responseData}></Custom6>
           </div>
         </div>
       </div>
--- src/components/templates/CustomTemplate/index.tsx
+++ src/components/templates/CustomTemplate/index.tsx
@@ -5,11 +5,11 @@ import Custom3 from "@/components/organism/Custom3";
 import Custom4 from "@/components/organism/Custom4";
 import Custom5 from "@/components/organism/Custom5";
 import CustomBGImage from "@/components/organism/CustomBGImage";
-import axios from "axios";
 import { useAtomValue } from "jotai";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import { customAtom } from "./JAtom";
+import getGPTData from "@/api/GPT";
 
 const CustomTamplate = () => {
   const router = useRouter();
@@ -19,58 +19,14 @@ const CustomTamplate = () => {
     setLevel(level + 1);
   };
 
-  // API 호출
-  const [loading, setLoading] = useState<boolean>(false);
-  const [error, setError] = useState<string | null>(null);
-
   const custom = useAtomValue(customAtom);
 
   const handleSubmit = async () => {
     console.log("12");
 
-    if (!custom.story) {
-      console.log("no story");
-      setError("Both story are required");
-      return;
-    }
-
-    if (!custom.image) {
-      console.log("no image");
-    } else {
-      const response = await axios.get(custom.image, {
-        responseType: "blob",
-      });
-
-      const formData = new FormData();
-      formData.append("file", response.data, "image.jpg");
-
-      setLoading(true); // 로딩 시작
-      setError(null); // 기존 에러 리셋
-
-      console.log("2");
-      try {
-        const res = await axios.post(
-          `http://113.198.229.153:201/api/tasks/?story=${encodeURIComponent(
-            custom.story + custom.taste
-          )}`,
-          formData,
-          {
-            headers: {
-              "Content-Type": "multipart/form-data",
-            },
-          }
-        );
-        localStorage.setItem("apiResponse", JSON.stringify(res.data)); // API 응답을 localStorage에 저장
-        setLoading(false); // 로딩 종료
-        console.log(JSON.stringify(res.data));
-        router.push("/custom/success"); // 성공 페이지로 이동
-      } catch (err) {
-        console.log("api error");
-        setError("Failed to send request");
-        setLoading(false); // 로딩 종료
-      }
-    }
+    router.push("/custom/success");
   };
+
   useEffect(() => {
     switch (level) {
       case 1:
