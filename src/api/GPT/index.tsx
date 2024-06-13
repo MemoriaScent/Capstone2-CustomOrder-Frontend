04dbe2911e9a9d601115dba69f26f5533f8a5960
Hun3431 <limdohun77@gmail.com>
2024-06-14 02:51:19 +0900

    [Fix] Api 에러메시지 나오도록 변경

--- src/api/GPT/index.tsx
+++ src/api/GPT/index.tsx
@@ -11,6 +11,21 @@ const getGPTData = async (text: string, image: string): Promise<any> => {
   const formData = new FormData();
   formData.append("file", response.data, "image.jpg");
 
+  // try {
+  //   const res = await axios.post(
+  //     `${process.env.NEXT_PUBLIC_GPT_SERVER_URL}:${
+  //       process.env.NEXT_PUBLIC_GPT_SERVER_PORT
+  //     }/api/tasks/gpt?story=${encodeURIComponent(text)}&img_caption=%20`,
+  //     {},
+  //     {
+  //       headers: {
+  //         Accept: "application/json",
+  //       },
+  //     }
+  //   );
+  //   return res.data;
+  //   // return JSON.stringify(res.data);
+  // }
   try {
     const res = await axios.post(
       `${process.env.NEXT_PUBLIC_GPT_SERVER_URL}:${
@@ -27,6 +42,10 @@ const getGPTData = async (text: string, image: string): Promise<any> => {
     // return JSON.stringify(res.data);
   } catch (err) {
     console.log(err);
+    if (axios.isAxiosError(err)) {
+      console.error("Axios error message:", err.message);
+      console.error("Axios error response:", err.response?.data);
+    }
   }
   return undefined;
 };
