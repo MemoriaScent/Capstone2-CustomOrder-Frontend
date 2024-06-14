308eec5a8b326e2b05834e90c7db6ecd2feb3067
Hun3431 <limdohun77@gmail.com>
2024-06-14 23:24:11 +0900

    [Refactor] CartTemplate 불필요한 헤더 제거

--- src/components/templates/CartTemplate/index.tsx
+++ src/components/templates/CartTemplate/index.tsx
@@ -1,15 +1,11 @@
 import CartInfo from "@/components/organism/Cart";
-import CartDetBtn from "@/components/molecule/CartDetBtn";
-import Inquiry11 from "@/components/organism/Inquiry";
 
 const CartInfoTemplate = () => {
-    return(
-        <div className="flex flex-col items-center justify-center mt-20 mx-auto">
-            <CartInfo/>
-            
-            
-        </div>
-    );
+  return (
+    <div className="flex flex-col items-center justify-center mt-20 mx-auto">
+      <CartInfo />
+    </div>
+  );
 };
 
-export default CartInfoTemplate
+export default CartInfoTemplate;
