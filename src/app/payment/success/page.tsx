c3bf9dede733b882ed607dbc857086f8c0cae22a
Hun3431 <limdohun77@gmail.com>
2024-06-14 13:42:35 +0900

    [Fix] 결제 로딩페이지 수정

--- src/app/payment/success/page.tsx
+++ src/app/payment/success/page.tsx
@@ -3,7 +3,18 @@ import { Suspense } from "react";
 
 const PayMentSuccess = () => {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense
+      fallback={
+        <div className="flex flex-col items-center justify-center mt-100">
+          <img
+            src="https://static.toss.im/lotties/loading-spot-apng.png"
+            width="120"
+            height="120"
+          />
+          <h2 className="title text-center">결제 요청까지 성공했어요.</h2>
+        </div>
+      }
+    >
       <PayMentSuccessPage></PayMentSuccessPage>
     </Suspense>
   );
