6d98d680532a5c9b40ab28e559d7c7bbb3964c0c
Hun3431 <limdohun77@gmail.com>
2024-06-15 19:23:39 +0900

    [Feat] API 호출 로딩화면 추가

--- src/components/templates/CustomSuccessTemplate/index.tsx
+++ src/components/templates/CustomSuccessTemplate/index.tsx
@@ -6,6 +6,7 @@ import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import { customAtom, gptImageFileAtom } from "../CustomTemplate/JAtom";
 import getGPTData from "@/api/GPT";
+import Label from "@/components/atoms/Label";
 
 const CustomSuccessTemplate = async () => {
   const router = useRouter();
@@ -67,8 +68,24 @@ const CustomSuccessTemplate = async () => {
 
   if (loading) {
     return (
-      <div className="flex flex-col justify-center items-center">
-        <div>Loading</div>
+      <div className="relative w-100vw h-66vw">
+        <CustomBGImage />
+        <div className="absolute w-full h-full flex justify-center">
+          <div className="absolute z-10 w-full h-full flex justify-center bg-black cursor-wait opacity-10"></div>
+          <div className=" flex flex-col items-center justify-center w-1300 h-45vw border-x border-t border-black bg-white">
+            <div className="z-30 bg-white border p-32 flex flex-col items-center rounded-[30px] cursor-wait ">
+              <Label className=" mb-20 pretendardNormalFont-16">
+                당신의 이야기를 향기로 변환하고 있어요.
+              </Label>
+              <img
+                src="/loading.svg"
+                width="20"
+                height="20"
+                className="animate-spin w-20 h-20 "
+              ></img>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
